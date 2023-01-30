52e632379fb8458ae8ede058810148043ca86d4b
Rishabh Malviya <malviyarishabh73@gmail.com>
2023-01-30 12:00:40 +0530

    Queue : Output screenshot added

--- Data Structures/Queue/Queue.java
+++ Data Structures/Queue/Queue.java
@@ -34,36 +34,37 @@ public class Queue {
             System.out.println("Press 0 : Create a New Queue");
             System.out.println("Press 1 : Enqueue an element");
             System.out.println("Press 2 : Dequeue an element");
-            System.out.println("Press 3 : Update an element");
-            System.out.println("Press 4 : Display/Traversal the LinkedList");
+            System.out.println("Press 3 : Front of Queue");
+            System.out.println("Press 4 : Display/Traversal the Queue");
             System.out.println("Press 5 : Exit");
             System.out.println("Enter your choice : ");
             choice = sc.nextInt();
             // Taking the choice from the user and performing the corresponding operation
             switch (choice) {
                 case 0:
-                    System.out.print("Please Enter the first Node Value: ");
+                    System.out.print("Please Enter the first Element: ");
                     head = createQueue(sc.nextInt());
                     break;
                 case 1:
                     if (head != null) {
-                        System.out.print("Please Enter the Node Value: ");
-                        head = enqueue(head, sc.nextInt());
+                        System.out.print("Please Enter the Element: ");
+                        enqueue(head, sc.nextInt());
                     } else {
-                        System.out.println("Please create a LinkedList first");
+                        System.out.println("Please create a Queue first");
                     }
                     break;
                 case 2:
-                    System.out.println("Please Enter the Node Value: ");
-                    int dequeuedElement = dequeue(head);
-                    System.out.println("Dequeued Element: " + dequeuedElement);
+                    if(head!=null) {
+                        head = dequeue(head);
+                    }
                     break;
                 case 3:
-                    System.out.print("Please Enter the Node Value: ");
-                    updateElement(head, sc.nextInt());
+                    if(head!=null)
+                        front(head);
                     break;
                 case 4:
-                    display(head);
+                    if(head!=null)
+                        display(head);
                     break;
                 case 5:
                     System.out.println("Program Terminated Successfully");
@@ -74,38 +75,34 @@ public class Queue {
         } while (choice != 5);
 
     }
-
-    private static Node enqueue(Node head, int nextInt) {
+    // Method to find the front of the queue
+    private static void front(Node head) {
+        if (head == null) {
+            System.out.println("Queue is empty");
+        } else {
+            System.out.println("The front element is: " + head.data);
+        }
+    }
+    // Method to enqueue an element
+    private static Node enqueue(Node head, int value) {
         Node temp = head;
         while (temp.next != null) {
             temp = temp.next;
         }
-        temp.next = new Node(nextInt);
+        temp.next = new Node(value);
         return head;
     }
-
-    private static int dequeue(Node head) {
+    // Method to dequeue an element
+    private static Node dequeue(Node head) {
         if (head == null) {
             System.out.println("Queue is empty");
-            return -1;
+            return head;
         }
-        int dequeuedElement = head.data;
+        System.out.println("Dequeued Element: " + head.data);
         head = head.next;
-        return dequeuedElement;
+        return head;
     }
-
-    private static void updateElement(Node head, int nextInt) {
-        Node temp = head;
-        while (temp != null) {
-            if (temp.data == nextInt) {
-                System.out.print("Please Enter the new Node Value: ");
-                temp.data = sc.nextInt();
-                break;
-            }
-            temp = temp.next;
-        }
-    }
-
+    // Method to display the queue
     private static void display(Node head) {
         Node temp = head;
         System.out.println("The Queue is: ");
@@ -115,21 +112,9 @@ public class Queue {
         }
         System.out.println();
     }
-
-    private static Node dequeue(Node head, int nextInt) {
-        if (head == null) {
-            System.out.println("Queue is empty");
-            return null;
-        } else {
-            Node temp = head;
-            head = head.next;
-            System.out.println("The deleted element is: " + temp.data);
-            return head;
-        }
-    }
-
-    private static Node createQueue(int nextInt) {
-        Node head = new Node(nextInt);
+    // Method to create a queue
+    private static Node createQueue(int firstValue) {
+        Node head = new Node(firstValue);
         return head;
     }
 
