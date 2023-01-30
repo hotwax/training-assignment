29959152c45dc7b29a7d689740000cd8b6c4ee3a
Aditya Rao <adityarao122k@gmail.com>
2023-01-30 17:03:54 +0530

    Added Exception Handling, Menu in Chaining Hashing technique.

--- Module 1/Hashing Techniques/Chaining.java
+++ Module 1/Hashing Techniques/Chaining.java
@@ -1,172 +1,164 @@
-import java.util.ArrayList;
-import java.util.Objects;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class Chaining {
+    static class Pair {
+        // key
+        int key;
+        int value;
+        Pair next;
 
-// A node of chains
-class HashNode<K, V> {
-    K key;
-    V value;
-    final int hashCode;
-
-    // Reference to next node
-    HashNode<K, V> next;
-
-    // Constructor
-    public HashNode(K key, V value, int hashCode) {
-        this.key = key;
-        this.value = value;
-        this.hashCode = hashCode;
-    }
-}
-
-// Class to represent entire hash table
-class Chaining<K, V> {
-    // bucketArray is used to store array of chains
-    private ArrayList<HashNode<K, V>> bucketArray;
-
-    // Current capacity of array list
-    private int numBuckets;
-
-    // Current size of array list
-    private int size;
-
-    // empty chains.
-    public Chaining() {
-        bucketArray = new ArrayList<>();
-        numBuckets = 10;
-        size = 0;
-
-        // Create empty chains
-        for (int i = 0; i < numBuckets; i++)
-            bucketArray.add(null);
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public boolean isEmpty() {
-        return size() == 0;
-    }
-
-    private final int hashCode(K key) {
-        return Objects.hashCode(key);
-    }
-
-    // This implements hash function to find index for a key
-    private int getBucketIndex(K key) {
-        int hashCode = hashCode(key);
-        int index = hashCode % numBuckets;
-        // key.hashCode() could be negative.
-        index = index < 0 ? index * -1 : index;
-        return index;
-    }
-
-    // Method to remove a given key
-    public V remove(K key) {
-        // Apply hash function to find index for given key
-        int bucketIndex = getBucketIndex(key);
-        int hashCode = hashCode(key);
-        // Get head of chain
-        HashNode<K, V> head = bucketArray.get(bucketIndex);
-
-        // Search for key in its chain
-        HashNode<K, V> prev = null;
-        while (head != null) {
-            // If Key found
-            if (head.key.equals(key) && hashCode == head.hashCode)
-                break;
-
-            // Else keep moving in chain
-            prev = head;
-            head = head.next;
+        Pair(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
         }
-
-        // If key was not there
-        if (head == null)
-            return null;
-
-        size--;
-
-        // Remove key
-        if (prev != null)
-            prev.next = head.next;
-        else
-            bucketArray.set(bucketIndex, head.next);
-
-        return head.value;
     }
 
-    // Returns value for a key
-    public V get(K key) {
-        // Find head of chain for given key
-        int bucketIndex = getBucketIndex(key);
-        int hashCode = hashCode(key);
+    static class hashMap {
+        int size;
+        Pair[] arr;
+        int collision;
 
-        HashNode<K, V> head = bucketArray.get(bucketIndex);
-
-        // Search key in chain
-        while (head != null) {
-            if (head.key.equals(key) && head.hashCode == hashCode)
-                return head.value;
-            head = head.next;
-        }
-
-        // If key not found
-        return null;
-    }
-
-    // Adds a key value pair to hash
-    public void add(K key, V value) {
-        // Find head of chain for given key
-        int bucketIndex = getBucketIndex(key);
-        int hashCode = hashCode(key);
-        HashNode<K, V> head = bucketArray.get(bucketIndex);
-
-        // Check if key is already present
-        while (head != null) {
-            if (head.key.equals(key) && head.hashCode == hashCode) {
-                head.value = value;
-                return;
+        hashMap(int size) {
+            this.size = size;
+            arr = new Pair[size];
+            for (int i = 0; i < size; i++) {
+                arr[i] = null;
             }
-            head = head.next;
+            collision = 0;
         }
 
-        // Insert key in chain
-        size++;
-        head = bucketArray.get(bucketIndex);
-        HashNode<K, V> newNode = new HashNode<K, V>(key, value, hashCode);
-        newNode.next = head;
-        bucketArray.set(bucketIndex, newNode);
+        // Function to insert a key value pair in the hash map
+        public int hashFunction(int key) {
+            return key % size;
+        }
 
-        // If load factor goes beyond threshold, then double hash table size
-        if ((1.0 * size) / numBuckets >= 0.7) {
-            ArrayList<HashNode<K, V>> temp = bucketArray;
-            bucketArray = new ArrayList<>();
-            numBuckets = 2 * numBuckets;
-            size = 0;
-            for (int i = 0; i < numBuckets; i++)
-                bucketArray.add(null);
+        // Function to insert a key value pair in the hash map
+        public void insert(int key, int value) {
+            int index = hashFunction(key);
+            Pair newNode = new Pair(key, value);
+            if (arr[index] == null) {
+                arr[index] = newNode;
+            } else {
+                Pair temp = arr[index];
+                while (temp.next != null) {
+                    temp = temp.next;
+                }
+                collision++;
+                temp.next = newNode;
+            }
+        }
 
-            for (HashNode<K, V> headNode : temp) {
-                while (headNode != null) {
-                    add(headNode.key, headNode.value);
-                    headNode = headNode.next;
+        // Function to search a key in the hash map
+        public void delete(int key) {
+            int index = hashFunction(key);
+            Pair temp = arr[index];
+            if (temp.key == key) {
+                arr[index] = temp.next;
+            } else {
+                while (temp.next != null) {
+                    if (temp.next.key == key) {
+                        temp.next = temp.next.next;
+                        break;
+                    }
+                    temp = temp.next;
                 }
             }
         }
+
+        // Function to get a value from the hash map
+        public int get(int key) {
+            int index = hashFunction(key);
+            Pair temp = arr[index];
+            while (temp != null) {
+                if (temp.key == key) {
+                    return temp.value;
+                }
+                temp = temp.next;
+            }
+            return -1;
+        }
+
+        // Function to display the hash map
+        public void display() {
+            for (int i = 0; i < size; i++) {
+                Pair temp = arr[i];
+                while (temp != null) {
+                    System.out.print(temp.key + " -> " + temp.value);
+                    temp = temp.next;
+                }
+                System.out.println();
+            }
+        }
+
+        // Function to get all the collisions
+        public int getCollision() {
+            return collision;
+        }
     }
 
-    // Main Method
     public static void main(String[] args) {
-        Chaining<String, Integer> map = new Chaining<>();
-        map.add("this", 10);
-        map.add("coder", 20);
-        map.add("those", 40);
-        map.add("hi", 50);
-        System.out.println(map.size());
-        System.out.println(map.remove("this"));
-        System.out.println(map.remove("this"));
-        System.out.println(map.size());
-        System.out.println(map.isEmpty());
+        hashMap map = null;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the hashmap");
+        map = new hashMap(sc.nextInt());
+        int choice;
+        do {
+            System.out.println();
+            System.out.println("Kindly choose an option from the DashBoard given below:");
+            System.out.println("Press 1 : Insert  Value");
+            System.out.println("Press 2 : Delete Value");
+            System.out.println("Press 3 : Display the Hashmap");
+            System.out.println("Press 4 : Get All Collisions");
+            System.out.println("Press 5 : Get Value from Key");
+            System.out.println("Press 6 : Exit");
+            System.out.println("Enter your choice : ");
+            choice = sc.nextInt();
+            try {
+                switch (choice) {
+                case 1:
+                    if (map != null) {
+                        System.out.print("Please Enter the key: ");
+                        int key = sc.nextInt();
+                        System.out.print("Please Enter the value: ");
+                        int value = sc.nextInt();
+                        map.insert(key, value);
+                    }
+                    break;
+                case 2:
+                    if (map != null) {
+                        System.out.print("Please Enter the key: ");
+                        map.delete(sc.nextInt());
+                        System.out.println("Deleted");
+                    }
+                    break;
+                case 3:
+                    if (map != null)
+                        map.display();
+                    break;
+                case 4:
+                    if (map != null) {
+                        System.out.println("Number of collisions: " + map.getCollision());
+                    }
+                    break;
+                case 5:
+                    if (map != null) {
+                        System.out.print("Please Enter the key: ");
+                        int key1 = sc.nextInt();
+                        System.out.println("Value: " + map.get(key1));
+                    }
+                    break;
+                case 6:
+                    System.out.println("Program Terminated");
+                    break;
+                default:
+                    System.out.println("Invalid Choice");
+                    break;
+            }
+            }
+            catch(InputMismatchException ex) {
+                System.out.println(ex.getMessage());
+            }
+        } while (choice != 6);
     }
 }
