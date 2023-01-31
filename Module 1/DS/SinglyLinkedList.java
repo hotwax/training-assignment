5f15f9e47646ee77ae32a6ddf5f68c659a5c6b37
Aditya Rao <adityarao122k@gmail.com>
2023-01-31 11:35:37 +0530

    Added Exception Handling for LinkedList Data Structure.

--- Module 1/DS/SinglyLinkedList.java
+++ Module 1/DS/SinglyLinkedList.java
@@ -1,201 +1,200 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
-
 public class SinglyLinkedList {
-
-    Node head; // head of list
-
-    // Node class.
     static class Node {
-
+        // Data of the node
         int data;
+        // Pointer to the next node
         Node next;
 
-        // Constructor
-        Node(int d) {
-            data = d;
-            next = null;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
+    // Method to create a new linked list
+    static Node createList(int value) {
+        return new Node(value);
+    }
 
-    // Insert Method
-    public static SinglyLinkedList insert(SinglyLinkedList list, int d) {
+    // Method to insert a new node to the linked list
+    static Node insert(Node head, int data) {
+        Node temp = new Node(data);
+        Node node = head;
+        while (node.next != null) {
+            node = node.next;
+        }
+        node.next = temp;
+        return head;
+    }
 
-        Node new_node = new Node(d);
-        new_node.next = null;
+    // Method to display the complete linked list.
+    static void displayList(Node head) {
+        Node temp = head;
+        System.out.println("The Linked List is : ");
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
 
-        // Case 1: If Linked List is empty
-        if (list.head == null) {
-            list.head = new_node;
+    // Method to delete a node in the linked list
+    static Node deleteNode(Node head, int data) {
+        Node temp = head;
+        while (temp.next != null) {
+            if (temp.next.data == data) {
+                temp.next = temp.next.next;
+                break;
+            }
+            temp = temp.next;
+        }
+        displayList(head);
+        return head;
+    }
+
+    // Sort Method : Used merge sort to sort the list. 
+    static void updateNode(Node head, int data) {
+        Node temp = head;
+        while (temp.next != null) {
+            if (temp.next.data == data) {
+                System.out.println("Please Enter the new value of the node: ");
+                temp.next.data = sc.nextInt();
+                break;
+            }
+            temp = temp.next;
+        }
+        displayList(head);
+    }
+
+    // Merge sort on Linked List
+    static Node sortList(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node mid = getMid(head);
+        Node nextToMid = mid.next;
+        mid.next = null;
+        Node left = sortList(head);
+        Node right = sortList(nextToMid);
+        return merge(left, right);
+    }
+
+    // Function to get the middle node of the linked list
+    static Node getMid(Node head) {
+        if (head == null) {
+            return head;
+        }
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null) {
+            fast = fast.next;
+            if (fast != null) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+        }
+        return slow;
+    }
+
+    // Function to merge two sorted linked lists
+    static Node merge(Node a, Node b) {
+        Node result = null;
+        if (a == null) {
+            return b;
+        }
+        if (b == null) {
+            return a;
+        }
+        if (a.data <= b.data) {
+            result = a;
+            result.next = merge(a.next, b);
         } else {
-            // Otherwise we traverse till the end of the linked list.
-            Node last = list.head;
-            while (last.next != null) {
-                last = last.next;
+            result = b;
+            result.next = merge(a, b.next);
+        }
+        return result;
+    }
+
+    // Function to search a node in the linked list
+    static void searchNode(Node head, int data) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == data) {
+                System.out.println("Node Found");
+                return;
             }
-            // Insert at the end.
-            last.next = new_node;
+            temp = temp.next;
         }
-        return list;
+        System.out.println("Node Not Found");
     }
+    // Class for the node of the linked list
 
-    // Delete Method 
-    public static SinglyLinkedList delete(SinglyLinkedList list, int d) {
-
-        Node cNode = list.head, prev = null;
-
-        // Case 1: If head node is to be deleted.
-        if (cNode != null && cNode.data == d) {
-            list.head = cNode.next;
-            System.out.println(d + " Data is deleted.");
-            return list;
-        }
-
-        // Case 2: Finding the element while traversing the list.
-        while (cNode != null && cNode.data != d) {
-            prev = cNode;
-            cNode = cNode.next;
-        }
-
-        if (cNode != null) {
-            prev.next = cNode.next;
-            System.out.println(d + " Data is deleted.");
-        }
-
-        // Case 3: If element not present in the list.
-        if (cNode == null) {
-            System.out.println(d + " Element not found.");
-        }
-
-        return list;
-    }
-
-    // Update Method or Replace Method
-    public static SinglyLinkedList update(SinglyLinkedList list, int d, int newD) {
-
-        Node cNode = list.head, prev = null;
-
-        // Case 1: If element is present at the head.
-        if (cNode != null && cNode.data == d) {
-            cNode.data = newD;
-            System.out.println(d + " found and updated");
-            return list;
-        }
-
-        // Case 2: Finding element by traversing the list.
-        while (cNode != null && cNode.data != d) {
-            prev = cNode;
-            cNode = cNode.next;
-        }
-
-        if (cNode != null) {
-            cNode.data = newD;
-            System.out.println(d + " found and updated");
-        }
-
-        // Case 3: If element is not in the list.
-        if (cNode == null) {
-            System.out.println(d + " not found");
-        }
-
-        return list;
-    }
-    // Sort Method
-    public static SinglyLinkedList sort(SinglyLinkedList list) {
-        Node currNode = list.head;
-        Node nextNode = list.head.next;
-        int temp;
-        while (currNode != null) {
-            while (nextNode != null) {
-                if (currNode.data > nextNode.data) {
-                    temp = currNode.data;
-                    currNode.data = nextNode.data;
-                    nextNode.data = temp;
-                }
-                nextNode = nextNode.next;
-            }
-            currNode = currNode.next;
-            nextNode = currNode.next;
-        }
-        return list;
-    }
-
-    // Print method: Basically prints linked list after every operation.
-    public static void print(SinglyLinkedList list) {
-        Node currNode = list.head;
-        System.out.print(" LinkedList:  ");
-
-        // Traverse through the LinkedList
-        while (currNode != null) {
-            // Print the data at current node
-            System.out.print(currNode.data + "->");
-            // Go to next node
-            currNode = currNode.next;
-        }
-        System.out.println("null");
-    }
-
-    // Main Method
+    // For taking input.
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // Creating a LinkedList
-        SinglyLinkedList list = new SinglyLinkedList();
-        Boolean flag = true;
-        while (flag) {
-            System.out.println("Choose an option from the Dashboard given below: ");
-            System.out.println("1. Insert");
-            System.out.println("2. Delete");
-            System.out.println("3. Update");
-            System.out.println("4. Sorting");
-            System.out.println("5. Exit");
+        // Create a blank linked list
+        Node head = null;
+        System.out.print("Please Enter the first Node Value: ");
+        head = createList(sc.nextInt());
+        // Variable to store the choice of the user
+        int choice;
+        do {
+            // Printing the menu for the user
             System.out.println();
-            Scanner sc = new Scanner(System.in);
-            int ch = sc.nextInt();
-            if (ch < 1 || ch > 5) 
-            {
-                System.out.println("Invalid choice");
-            } 
-            else 
-            {
-                try 
-                {
-                    switch (ch) {
-                        case 1:
-                            System.out.println("Enter the value to be inserted");
-                            int i_value = sc.nextInt();
-                            list = insert(list, i_value);
-                            break;
-                        case 2:
-                            System.out.println("Enter the value to be deleted");
-                            int d_value = sc.nextInt();
-                            list = delete(list, d_value);
-                            break;
-                        case 3:
-                            System.out.println("Enter the value of the node which is to be updated.");
-                            int s_value = sc.nextInt();
-                            int n_value = sc.nextInt();
-                            list = update(list, s_value, n_value);
-                            break;
-                        case 4:
-                            System.out.println("Sorting the list");
-                            list = sort(list);
-                            break;
-                        case 5:
-                            flag = false;
-                            break;
-                    }
-                    print(list);
-                    if (flag == false) {
-                        System.out.println("Exiting the program.");
-                    }
+            System.out.println("Select an option from the dashboard givene below:");
+            System.out.println("1 : Insert ");
+            System.out.println("2 : Delete ");
+            System.out.println("3 : Update ");
+            System.out.println("4 : Sort LinkedList");
+            System.out.println("5 : Display LinkedList");
+            System.out.println("6 : Search in LinkedList");
+            System.out.println("7 : Exit");
+            System.out.println("------------------------------------------");
+            System.out.println("Enter your choice : ");
+            choice = sc.nextInt();
+            // Taking the choice from the user and performing the corresponding operation
+            try {
+                switch (choice) {
+                    case 1:
+                        if (head != null) {
+                            System.out.print("Enter the Node Value: ");
+                            head = insert(head, sc.nextInt());
+                        }
+                        break;
+                    case 2:
+                        System.out.print("Enter the Node Value: ");
+                        head = deleteNode(head, sc.nextInt());
+                        break;
+                    case 3:
+                        System.out.print(" Enter the Node Value which is to be updated: ");
+                        updateNode(head, sc.nextInt());
+                        break;
+                    case 4:
+                        head = sortList(head);
+                        break;
+                    case 5:
+                        displayList(head);
+                        break;
+                    case 6:
+                        System.out.print(" Enter the value to be searched: ");
+                        searchNode(head, sc.nextInt());
+                        break;
+                    case 7:
+                        System.out.println("Exiting Program.");
+                        break;
+                    default:
+                        System.out.println("Invalid Input");
                 }
-
-            catch(InputMismatchException ex) {
+            } catch(InputMismatchException ex) {
                 System.out.println(ex.getMessage());
             }
-        }
+        } while (choice != 7);
 
     }
-}
+
+    
+
 }
