b2dab201feda4305324264737546f8f953a5d175
Dhruv Singh <singhdhruv861@gmail.com>
2023-02-02 16:33:13 +0530

    performed changes in ord count program (problem1).

--- word_count/CountWords.java
+++ word_count/CountWords.java
@@ -15,66 +15,99 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.*;
+import org.jsoup.nodes.Document;
 
 public class CountWords {
     public static void main(String args[]) throws Exception{
-        File inputFile=new File("E:\\Work\\java work\\Module2\\src\\word_count\\input.txt");
-        BufferedReader br=new BufferedReader(new FileReader(inputFile));
-        
+        //list to store given urls
         ArrayList<String> inputUrls=new ArrayList<>();
-        ArrayList<String> inputWords=new ArrayList<>();
-        
+        //list to store given words to search in url.
+        ArrayList<String> inputWords=new ArrayList<>();     
         //created a arrayList of hashMap to store occurence of every word in every url.
-        ArrayList<HashMap<String,Integer>> maps=new ArrayList<>();
+        ArrayList<HashMap<String,Integer>> wordsFreq=new ArrayList<>();
+
         
+        //====================================================================================================================
+        //taking input urls from urls.txt file.
+        //created an object of File class to perform operations on input files.
+        File inputFile=new File("E:\\Work\\java work\\Module2\\src\\word_count\\urls.txt");
+        
+        //passing the object of File in FileReaded to read file and then pass the object of FileReader to BufferedReader
+        BufferedReader bufferedReader=new BufferedReader(new FileReader(inputFile));
+        //every url is stored as a new string in inputWords arraylist
         while(true){
-            String temp=br.readLine();
-            if(temp==null) break;
-            inputUrls.add(temp);
-            maps.add(new HashMap<>());
+            String currLine=bufferedReader.readLine();
+            //if cuurLine is null, it means we have read all characer from the urls.txt file.
+            //that's why we need to break the loop
+            if(currLine==null) break;
+            inputUrls.add(currLine);
+            //creating object of hashMap<> for every url present in urls.txt.
+            wordsFreq.add(new HashMap<>());
         }
+        //closing the Buffered Reader
+        bufferedReader.close();
+        
+        //====================================================================================================================
+        //taking input words from words.url.
+        //recreate File object to perform operations on words.txt file.
         inputFile=new File("E:\\Work\\java work\\Module2\\src\\word_count\\words.txt");
         
-        for(int i=0;i<maps.size();i++){
-            br=new BufferedReader(new FileReader(inputFile));
-            while(true){
-            String temp=br.readLine();
+        //recreate buffered reader object to read content of words.txt file.
+        bufferedReader=new BufferedReader(new FileReader(inputFile));
+        while(true){
+            String temp=bufferedReader.readLine();
+            //if cuurLine is null, it means we have read all characer from the words.txt file.
+            //that's why we need to break the loop
             if(temp==null) break;
+            //adding words in inputWords ArrayList.
             inputWords.add(temp);
-            maps.get(i).put(temp,0);
         }
-        }
+        // clossing the BufferedReader
+        bufferedReader.close();
+        
+        //====================================================================================================================
         for(int i=0;i<inputUrls.size();i++){
-            String s=inputUrls.get(i);
-            URL url=new URL(s);
-            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-            String inputLine;
-            String text="";
-            while ((inputLine = in.readLine()) != null)
-                text+=inputLine;
-            in.close();
+            //single url from ArrayList of urls
+            String url=inputUrls.get(i);
             
-            //filtering
-            String filteredText=Jsoup.parse(text).text();
+            //-----------------------------------------------------------------------------------------------------------
+            //FILTERING
+            //created an object of Document class store all content of url fetched using Jsoup
+            Document newdoc = Jsoup.connect(url).get();
+            //we need only text from whole document.
+            String text = newdoc.body().text();
+            //we don't need anything other than alphabates(small and capital).
+            //that's why we repalce all other than alphabates from empty space
+            text = text.replaceAll("[^a-zA-Z\\w]", " ");
             
-            for(String word:inputWords){
-                Pattern pattern = Pattern.compile("\\W*([ ]"+word+"[. ])\\W*");
-                Matcher matcher = pattern.matcher(filteredText);
-                String finalRes="";
-                while(matcher.find()){
-                    String key=matcher.group();
-                    maps.get(i).put(word,maps.get(i).get(word)+1);
+            //fetchedWords is an array of String which store all words seperated by an empty space. 
+            String fetchedWords[]=text.split(" ");
+
+            //-----------------------------------------------------------------------------------------------------------
+            //SEARCHING
+            //iterating over input words
+            for(String word_to_Search : inputWords){
+                wordsFreq.get(i).put(word_to_Search,0);
+                //iterating over ords fetched from url
+                for(String fetchedWord : fetchedWords){
+                    //if we find the exact same word that we want ro search then
+                    //fetch the specific hashMap from wordsFreq ArrayLits and increase the 
+                    //frequency of the word_to_search by 1
+                    if(word_to_Search.equalsIgnoreCase(fetchedWord)){
+                        wordsFreq.get(i).put(word_to_Search,wordsFreq.get(i).get(word_to_Search)+1);
+                    }
                 }
-                
             }
-            List<Map.Entry<String,Integer>> list=new ArrayList<>(maps.get(i).entrySet());
+            //-----------------------------------------------------------------------------------------------------------
+            //Sorting the map on the basis of values(frequencies) in decreasing order
+            List<Map.Entry<String,Integer>> list=new ArrayList<>(wordsFreq.get(i).entrySet());
             Collections.sort(list,new Comparator<Map.Entry<String,Integer>>(){
                 @Override
                 public int compare(Map.Entry<String,Integer> m1,Map.Entry<String,Integer> m2){
                     return m2.getValue()-m1.getValue();
                 }
             });
-            
+            //printing the first 3 highest frequency words.
             System.out.println(inputUrls.get(i));
             for(int j=0;j<3;j++){
                 System.out.println(list.get(j).getKey()+" = "+list.get(j).getValue());
@@ -83,24 +116,22 @@ public class CountWords {
             System.out.println("===============");
             System.out.println();
         }
+        //==================================================================================================================
+        
+        
         
         //merging all maps
-        for(int i=1;i<maps.size();i++){
-            maps.get(i).forEach((key, value) ->
-        maps.get(0).merge(key, value, (v1, v2) -> v1 + v2) );
+        //merging all maps input into the first map (at index 0)
+        for(int i=1;i<wordsFreq.size();i++){
+            wordsFreq.get(i).forEach((key, value) ->
+                    wordsFreq.get(0).merge(key, value, (v1, v2) -> v1 + v2) );
         }
         
-        List<Map.Entry<String,Integer>> list=new ArrayList<>(maps.get(0).entrySet());
-            Collections.sort(list,new Comparator<Map.Entry<String,Integer>>(){
-                @Override
-                public int compare(Map.Entry<String,Integer> m1,Map.Entry<String,Integer> m2){
-                    return m2.getValue()-m1.getValue();
-                }
-            });
-            System.out.println("merged output of all urls");
-            for(int j=0;j<list.size();j++){
-                System.out.println(list.get(j).getKey()+" = "+list.get(j).getValue());
-            }
+        //printing merged output for every url
+        System.out.println("merged output of all urls");
+        for(Map.Entry<String,Integer> entry: wordsFreq.get(0).entrySet()){
+            System.out.println(entry.getKey()+" = "+entry.getValue());
+        }
         
     }
 }
