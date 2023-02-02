f57f03bde210b55f06a8a70af108d62dbca129ae
Rishabh Malviya <malviyarishabh73@gmail.com>
2023-02-02 15:28:53 +0530

    Solved Problem 1 : Web Scrapping

---
+++ Module-2/src/Problem1/WebScrapping.java
@@ -0,0 +1,124 @@
+package Problem1;// Importing JSoup Library
+/*
+* Jsoup is a Java Library to Parsing HTML to Java*/
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+// Pair class : to store key,value in array
+class Pair{
+    String word;
+    int freq;  // frequency
+
+    public Pair(String word, int freq) {
+        this.word = word;
+        this.freq = freq;
+    }
+    @Override
+    public String toString() {
+        return "Word: " + word + "-" + freq;
+    }
+}
+
+public class WebScrapping {
+    public static void main(String[] args) throws Exception {
+        Map<String, Integer> map = new HashMap<>();
+
+
+        String wordReader;
+        try {
+            // Get the current working directory
+            String homeDir = System.getProperty("user.dir");
+            //  Create a file to get words from the file Words.txt
+            File wordsFile = new File(homeDir + "/src/Problem1/Words.txt");
+            BufferedReader wordBuffer = new BufferedReader(new FileReader(wordsFile));
+            ArrayList<String> words = new ArrayList<>();
+            //  Create a file to get words from the file Words.txt
+            while ((wordReader = wordBuffer.readLine()) != null) {
+                map.put(wordReader, 0);
+                words.add(wordReader);
+            }
+
+            // Create a file to get urls from the file urls.txt
+            File url = new File(homeDir + "/src/Problem1/URL.txt");
+            // Using BufferedReader to read url from urls.txt
+            BufferedReader urlBuffer = new BufferedReader(new FileReader(url));
+
+
+            System.out.println("------------- Output #1 --------------");
+            //reading urls line by line
+            String urlReader;
+            while ((urlReader = urlBuffer.readLine()) != null) {
+            /* Jsoup has `connect` method to that accepts url as string and uses `Connection` Interface behind
+               the scene to fetch a `Connection`  Object Reference : https://jsoup.org/apidocs/org/jsoup/Connection.html
+             */
+                /*  `.get()` parsed the result to Document*/
+                Document doc = Jsoup.connect(urlReader).get();
+                // Body method get the <body></body> tag and `.text()` extracts text from HTML tag
+                String text = doc.body().text();
+                System.out.println("---------------------------------------");
+                System.out.println("For URL : " + urlReader);
+
+                ArrayList<Pair> list = new ArrayList<>();
+                for (String word : words) {
+                    int count = getCount(word, text);
+                    // Adding frequencies
+                    list.add(new Pair(word, count));
+                    if (map.containsKey(word) && count > map.get(word)) map.put(word, count);
+                    else if (!map.containsKey(word)) map.put(word, count);
+                }
+                list.sort((pair1, pair2) -> pair2.freq - pair1.freq);
+                for (int index = 0; index < 3; index++) {
+                    System.out.println(list.get(index).toString());
+                }
+
+            }
+            // Comparator to sort the hashmap by values (Frequencies)
+            Comparator valueComparator = new Comparator() {
+                // k1 and k2 are objects are temporary variable to get the comparator to sort
+                public int compare(Object k1, Object k2) {
+                    int comp = map.get(k2).compareTo(map.get(k1));
+                    if (comp == 0) return 1;
+                    else return comp;
+                }
+            };
+            TreeMap<String, Integer> sortedMap = new TreeMap<String, Integer>(valueComparator);
+
+            sortedMap.putAll(map);
+
+            System.out.println("------------- Output #2 --------------");
+            for (Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
+                System.out.println(entry.getKey() + "-" + entry.getValue());
+            }
+            System.out.println("---------------------------------------");
+            // Handling Exception for file
+        } catch (IOException e){
+            System.out.println("File not Found!!");
+            e.printStackTrace();
+            // Handling any other Exception : Jsoup
+        } catch (Exception e){
+            System.out.println("Exception :" + e.getMessage());
+        }
+    }
+
+//    Function to count the occurrence of each word in the file
+    public static int getCount(String word, String text) {
+        String[] wordsArray = text.split(" ");
+        int count = 0;
+        for (String eachWord : wordsArray) {
+            if (eachWord.equalsIgnoreCase(word)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
