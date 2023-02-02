10c136a9f486c331a2e63dde58ef39b3534576b4
Rishabh Malviya <malviyarishabh73@gmail.com>
2023-02-02 10:53:04 +0530

    Solved Problem 4 :  Serialization and Deserialization

---
+++ Module-2/src/Problem1/Problem.java
@@ -0,0 +1,11 @@
+/*
+* 1. Problem 1
+* Creating a Java to Problem that fetches data from different URLs and performs
+* */
+
+package Problem1;
+public class Problem {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Module-2/src/Problem4/DeserialiazationTest.java
@@ -0,0 +1,41 @@
+package Problem4;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+class DeserialiazationTest {
+    public static void main(String[] args) {
+        // Defining the student List for storing the Student Objects
+        ArrayList<Student> students = new ArrayList();
+        // getting the current working directory
+        String currDir = getDirectory();
+        try {
+            // getting serialized file in current directory
+            FileInputStream inFile = new FileInputStream(currDir+"output1.ser");
+            // Creating a ObjectInputStream to get Objects
+            ObjectInputStream inObject =  new ObjectInputStream(inFile);
+            // storing object list to students
+            students = (ArrayList<Student>) inObject.readObject();
+            // closing the fileInputStream
+            inFile.close();
+            // closing objectInputStream
+            inObject.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+           students.forEach(student -> System.out.println(student.toString()));
+        }
+    }
+    // Function to get current working directory
+    private static String getDirectory(){
+        // Getting the project Directory
+        String currDir = System.getProperty("user.dir");
+        String newDir;
+        // Replacing all "\" to "\\" and adding the subdirectory
+        newDir = currDir.replaceAll("\\\\", "\\\\\\\\") + "\\src\\Problem4\\";
+        return newDir;
+    }
+
+}
