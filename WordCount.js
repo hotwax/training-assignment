92874a1a715325058cb8c15177d45ffbc418187d
Nidhi pal <nidhipalmms@gmail.com>
2023-03-01 13:40:34 +0530

    applied async and await to axios code

--- WordCount.js
+++ WordCount.js
@@ -7,58 +7,70 @@ const wordsArray = fileHandler.readFileSync("words.txt", "utf-8").split("\n"); /
 
 let wordsCountForAllUrls = new Map(); //map to count words frequencies across all urls
 
-urlsArray.forEach((url) => { //loop through urls
-  axios.get(url).then((response) => { //using axios to fetch url data 
-    let html = response.data; //html content of the site
-    let wordsFromUrl = htmlToText(html).split("\n").join(" "); //html to text
-    
-    let wordsCountForOneUrl = new Map(); //map to count words frequencies on one site
 
-    wordsArray.forEach( (wordFromFile) => { //loop through words
-      const regExp = new RegExp(`${wordFromFile}`, 'gi'); //regex for a global case-insensitive search
-      const matchedWords = wordsFromUrl.match(regExp) //returns an array of matched words from the site
+async function WordCount() {
+  for (let index = 0; index < urlsArray.length; index++) {
+    let url = urlsArray[index]
+    await axios.get(url).then((response) => { //using axios to fetch url data 
+      let html = response.data; //html content of the site
+      let wordsFromUrl = htmlToText(html).split("\n").join(" "); //html to text
 
-      if (matchedWords == null) {
-        wordsCountForOneUrl.set(wordFromFile, 0)
-      } else {
-        const wordCount = matchedWords.length //total count of the word in the site
-        wordsCountForOneUrl.set(wordFromFile, wordCount)
+      let wordsCountForOneUrl = new Map(); //map to count words frequencies on one site
 
-        //updating count of the word in the map which stores word total count
-        if (wordsCountForAllUrls.has(wordFromFile)) {
-          const oldCount = wordsCountForAllUrls.get(wordFromFile) 
-          wordsCountForAllUrls.set(wordFromFile, oldCount + wordCount);
-        }
-        else {
-          wordsCountForAllUrls.set(wordFromFile, wordCount)
+      for (let index = 0; index < wordsArray.length; index++) {
+        let wordFromFile = wordsArray[index]
+
+        const regExp = new RegExp(`${wordFromFile}`, 'gi'); //regex for a global case-insensitive search
+        const matchedWords = wordsFromUrl.match(regExp) //returns an array of matched words from the site
+
+        if (matchedWords == null) {
+          wordsCountForOneUrl.set(wordFromFile, 0)
+        } else {
+          const wordCount = matchedWords.length //total count of the word in the site
+          wordsCountForOneUrl.set(wordFromFile, wordCount)
+
+          //updating count of the word in the map which stores word total count
+          if (wordsCountForAllUrls.has(wordFromFile)) {
+            const oldCount = wordsCountForAllUrls.get(wordFromFile)
+            wordsCountForAllUrls.set(wordFromFile, oldCount + wordCount);
+          }
+          else {
+            wordsCountForAllUrls.set(wordFromFile, wordCount)
+          }
         }
       }
-    })
 
-    //sorting in descending order of words frequencies and then taking top 3 frequencies words
-    wordsCountForOneUrl = new Map([...wordsCountForOneUrl.entries()].sort(
-      function (a, b) {
-        return b[1] - a[1]
+      //sorting in descending order of words frequencies and then taking top 3 frequencies words
+      wordsCountForOneUrl = new Map([...wordsCountForOneUrl.entries()].sort(
+        function (a, b) {
+          return b[1] - a[1]
+        }
+      ).slice(0, 3))
+
+      console.log("Top 3 frequency words for url- " + url + "\n");
+
+      for (let [key, value] of wordsCountForOneUrl) {
+        console.log(key);
+        console.log(value);
       }
-    ).slice(0, 3))
+      console.log("-----------------------\n")
 
-    console.log("Top 3 frequency words for url- " + url+"\n");
+    });
 
-    for (let [key, value] of wordsCountForOneUrl) {
-      console.log(key);
-      console.log(value);
-    }
-    console.log("-----------------------\n")
+  }
 
-  });
-});
 
-console.log("Frequency of all words across all urls\n");
+  console.log("Frequency of all words across all urls\n");
 
-wordsCountForAllUrls.forEach((value, key) => {
-  console.log(key)
-  console.log(value)
-})
+  wordsCountForAllUrls.forEach((value, key) => {
+    console.log(key)
+    console.log(value)
+  })
+}
+
+WordCount()
+
+
 
 
 
