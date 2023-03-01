6f1f2b500f496a13e14cdd858fd2e5c2369bef79
Nidhi pal <nidhipalmms@gmail.com>
2023-03-01 16:44:48 +0530

    added filter, curried functions, arrow functions

--- WordCount.js
+++ WordCount.js
@@ -5,47 +5,66 @@ const { htmlToText } = require("html-to-text"); // to convert html of the site t
 const urlsArray = fileHandler.readFileSync("urls.txt", "utf-8").split("\n"); //get urls from urls.txt file
 const wordsArray = fileHandler.readFileSync("words.txt", "utf-8").split(","); //get words from words.txt file
 
+let wordsCountForOneUrl = new Map(); //map to count words frequencies on one site
 let wordsCountForAllUrls = new Map(); //map to count words frequencies across all urls
 
+const updatefrequencyForAUrl = (wordFromFile) => {
 
-async function WordCount() {
+  return function innerFunction(wordsFromUrl) {
+    const matchedWords = wordsFromUrl.filter((word) => {
+
+      const regExp = new RegExp(`${wordFromFile}`, 'gi');
+      return (
+        regExp.test(word)
+      )
+    })
+
+    if (matchedWords == null) {
+      wordsCountForOneUrl.set(wordFromFile, 0)
+    } else {
+      const wordCount = matchedWords.length //total count of the word in the site
+      wordsCountForOneUrl.set(wordFromFile, wordCount)
+
+      //updating count of the word in the map which stores word total count
+      if (wordsCountForAllUrls.has(wordFromFile)) {
+        const oldCount = wordsCountForAllUrls.get(wordFromFile)
+        wordsCountForAllUrls.set(wordFromFile, oldCount + wordCount);
+      }
+      else {
+        wordsCountForAllUrls.set(wordFromFile, wordCount)
+      }
+    }
+  }
+
+
+
+}
+
+const sortMapAndReturnTopThreeWords = () => {
+  //sorting in descending order of words frequencies and then taking top 3 frequencies words
+  wordsCountForOneUrl = new Map([...wordsCountForOneUrl.entries()].sort(
+    function (a, b) {
+      return b[1] - a[1]
+    }
+  ).slice(0, 3))
+}
+
+const WordCount = async () => {
   for (let index = 0; index < urlsArray.length; index++) {
     let url = urlsArray[index]
+
     await axios.get(url).then((response) => { //using axios to fetch url data 
       let html = response.data; //html content of the site
-      let wordsFromUrl = htmlToText(html).split("\n").join(" "); //html to text
+      let wordsFromUrl = htmlToText(html).split("\n").join(" ").split(" "); //html to text
 
-      let wordsCountForOneUrl = new Map(); //map to count words frequencies on one site
+      wordsCountForOneUrl = new Map();
 
       for (let index = 0; index < wordsArray.length; index++) {
         let wordFromFile = wordsArray[index]
-
-        const regExp = new RegExp(`${wordFromFile}`, 'gi'); //regex for a global case-insensitive search
-        const matchedWords = wordsFromUrl.match(regExp) //returns an array of matched words from the site
-
-        if (matchedWords == null) {
-          wordsCountForOneUrl.set(wordFromFile, 0)
-        } else {
-          const wordCount = matchedWords.length //total count of the word in the site
-          wordsCountForOneUrl.set(wordFromFile, wordCount)
-
-          //updating count of the word in the map which stores word total count
-          if (wordsCountForAllUrls.has(wordFromFile)) {
-            const oldCount = wordsCountForAllUrls.get(wordFromFile)
-            wordsCountForAllUrls.set(wordFromFile, oldCount + wordCount);
-          }
-          else {
-            wordsCountForAllUrls.set(wordFromFile, wordCount)
-          }
-        }
+        updatefrequencyForAUrl(wordFromFile)(wordsFromUrl)
       }
 
-      //sorting in descending order of words frequencies and then taking top 3 frequencies words
-      wordsCountForOneUrl = new Map([...wordsCountForOneUrl.entries()].sort(
-        function (a, b) {
-          return b[1] - a[1]
-        }
-      ).slice(0, 3))
+      sortMapAndReturnTopThreeWords()
 
       console.log("Top 3 frequency words for url- " + url + "\n");
 
