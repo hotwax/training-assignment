42090dbae18c464671e930447eebfee7186c9f96
Dhruv Singh <singhdhruv861@gmail.com>
2023-03-03 18:57:12 +0530

    added unit testing documentation and updated the code.

--- word_count/wordCount.js
+++ word_count/wordCount.js
@@ -11,13 +11,13 @@ let finalMap = [];
 
 async function main() {
     finalMap = [];
-    urls = {}
-    words = {}
+    urls = []
+    words = []
     try {
         //reding urls from inputURLs.txt file
         urls = fs.readFileSync('inputURLs.txt', 'utf8').split(/\r\n|\n/);
     }
-    catch (err) {
+    catch (error) {
         console.log('!!! INPUTURLS.TXT FILE NOT FOUND !!!')
     }
 
@@ -25,7 +25,7 @@ async function main() {
         //reding words from inputWords.txt file
         words = fs.readFileSync('inputWords.txt', 'utf8').split(/\r\n|\n/);
     }
-    catch (err) {
+    catch (error) {
         console.log('!!! INPUTWORDS.TXT FILE NOT FOUND !!!')
     }
 
@@ -40,12 +40,12 @@ async function getFromURL() {
     //finalMap = new Array();
     //loop to iterate over every urls
     //(not used forEach() method because it only works for synchronous functions.)
-    for (var i = 0; i < urls.length; i++) {
+    for (var url = 0; url < urls.length; url++) {
         //printing url
-        console.log('\n' + urls[i]);
+        console.log('\n' + urls[url]);
 
         //used axios get method to get data from urls 
-        await axios.get(urls[i])
+        await axios.get(urls[url])
             .then((response) => {
                 // this method filter the text from our response object and then performs
                 // further computations.
@@ -53,11 +53,11 @@ async function getFromURL() {
             })
             .catch((error) => {
                 //print this if url is not found or incorrect
-                console.log('!!! URL NOT FOUND !!!')
+                console.log('URL NOT FOUND !!!')
             })
     }
     //sorting final merged map in descending order.
-    finalMap.sort((a, b) => { return b[1] - a[1]; })
+    finalMap.sort((first, second) => { return second[1] - first[1]; })
     console.log('\nMerged Results for all URLs');
     //printing the final sorted map
     finalMap.forEach((value) => { console.log(value[0] + " - " + value[1]) })
@@ -69,18 +69,18 @@ function filterResponse(response) {
     //map to handle words count for single url
     let map = []
     //storing the data of response in res
-    let res = response.data;
+    let results = response.data;
     //storing data without html tags and scripts.
-    let text = htmlToText(res).split(' ');
-
+    let text = htmlToText(results).split(' ');
     //removing the empty words.
     let filteredText = text.filter((word) => {
         return word != '';
     })
+    
     //for every word, counting its frequency in the fetched data
-    words.forEach((word) => { count(filteredText, word, map) });
+    words.forEach((word) => { getCount(filteredText, word, map) });
     //sort map in descending order.
-    map.sort(function (a, b) { return b[1] - a[1] });
+    map.sort(function (first, second) { return second[1] - first[1] });
     //printing only 3 top values with most frequencies.
     map.slice(0, 3).forEach((value) => {
         console.log(value[0] + " - " + value[1])
@@ -89,32 +89,32 @@ function filterResponse(response) {
 }
 
 //function to count the the frequency of words in fetched arrays and storing in map.
-function count(array, word, map) {
+function getCount(array, word, map) {
     //assigning frequency to 0
-    let res = 0;
+    let count = 0;
     //if word present in array are matched with our word then increase its frequency by 1.
     array
         .filter((arrayword) => { return arrayword.toLowerCase() === word.toLowerCase() })
-        .forEach(() => { res++ });
+        .forEach(() => { count++ });
 
     //push the word and its corresponding frequencies in map.
-    map.push([word, res]);
+    map.push([word, count]);
 
     //flag to know if word is already present in map
-    var flag = true;
+    var found = false;
 
     finalMap
         .filter((value) => { return value[0] == word })
         .every((value) => {
             //if we found the matching word then increment add new frequencies to previously stored frequencies.
-            value[1] += res;
-            flag = false;
+            value[1] += count;
+            found = true;
             return false;
         });
     //if flag is not present in map the push it
-    if (flag) finalMap.push([word, res]);
+    if (!found) finalMap.push([word, count]);
     //in the end return frequency.
-    return res;
+    return count;
 }
 
 //export the getFromURL function and finalMap array
