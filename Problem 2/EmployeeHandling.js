6984d9fb74406b1fc0a13a1789bd3773593c4cb0
Rishabh Malviya <malviyarishabh73@gmail.com>
2023-03-07 15:44:51 +0530

    Added Unit Testing in Problem 2

--- Problem 2/EmployeeHandling.js
+++ Problem 2/EmployeeHandling.js
@@ -18,13 +18,92 @@ class Employee {
     return `${this.name},${this.email},${this.age},${this.dob}`;
   }
 }
-const createEmployee = () => {
+
+// Function to check if the age is valid
+const checkAge = () => {
+  // Returning a promise to test the age
   return new Promise((resolve) => {
-    reader.question('Enter name: ', (name) => {
-      reader.question('Enter email: ', (email) => {
-        reader.question('Enter age: ', (age) => {
-          reader.question('Enter dob: ', (dob) => {
-            const employee = new Employee(name, email, age, dob);
+    // Reading the age from console
+    reader.question('Enter age: ', (age) => {
+      // Checking if the age is valid
+      // If age is invalid, calling the function again
+      if (!age || isNaN(age)) {
+        console.log('Invalid age');
+        resolve(checkAge());
+      } else if (age < 0 || age > 100) {
+        console.log('Invalid age');
+        resolve(checkAge());
+      }
+      // If age is valid, resolving the promise
+      resolve(age);
+    });
+  });
+};
+const checkName = () => {
+  // Returning a promise to test the name
+  return new Promise((resolve) => {
+    // Reading the name from console
+    reader.question('\nEnter the employee name: ', (name) => {
+      // Checking if the name is valid
+      // If name is invalid, calling the function again
+      if (!name) {
+        console.log('Invalid Name');
+        resolve(checkName);
+      }
+      // If name is valid, resolving the promise
+      resolve(name);
+    });
+  });
+};
+const checkEmail = () => {
+  // Returning a promise to test the email
+  return new Promise((resolve) => {
+    // Reading the email from console
+    reader.question('Enter email: ', (email) => {
+      // Regex for email validation
+      const regex =
+        /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+      // Checking if the email is valid
+      // If email is invalid, calling the function again
+      if (!email || (email && !regex.test(email))) {
+        console.log('Invalid email');
+        resolve(checkEmail());
+      }
+      // If email is valid, resolving the promise
+      resolve(email);
+    });
+  });
+};
+const checkDob = () => {
+  // Returning a promise to test the dob
+  return new Promise((resolve) => {
+    reader.question('Enter dob: ', (dob) => {
+      // Regex for date validation
+      const regex =
+        /^([0-9][0-9][0-9][0-9])-(0[0-9]||1[0-2])-([0-2][0-9]||3[0-1])$/;
+      // Checking if the dob is valid
+      // If dob is invalid, calling the function again
+      if (!dob || (dob && !regex.test(dob))) {
+        console.log('Invalid date');
+        resolve(checkDob());
+      }
+      // If dob is valid, resolving the promise
+      resolve(dob);
+    });
+  });
+};
+
+const createEmployee = () => {
+  // Returning a promise to create the employee
+  return new Promise((resolve) => {
+    // Calling the functions to check the name, email, age and dob
+    checkName().then((name) => {
+      checkEmail().then((email) => {
+        checkAge().then((age) => {
+          checkDob().then((dob) => {
+            // Creating the employee object
+            const employee = new Employee(name, email, parseInt(age), dob);
+            // Resolving the promise
             resolve(employee);
           });
         });
@@ -39,75 +118,92 @@ const PATH = process.cwd() + '\\employees.txt';
 
 let employees = [];
 const getEmployeesFromFile = () => {
-  fs.readFileSync(PATH, 'utf8')
-    .trim()
-    .split('\n')
-    .forEach((line) => {
-      const [name, email, age, date] = line.trim().split(',');
-      const employee = new Employee(
-        name,
-        email,
-        parseInt(age),
-        new Date(date).toLocaleDateString()
-      );
-      employees.push(employee);
-    });
-  choice();
+  // Reading the file and storing the employees in the employees array
+  try {
+    fs.readFileSync(PATH, 'utf8')
+      .trim()
+      .split('\n')
+      .forEach((line) => {
+        const [name, email, age, date] = line.trim().split(',');
+        const employee = new Employee(
+          name,
+          email,
+          parseInt(age),
+          new Date(date).toLocaleDateString()
+        );
+        employees.push(employee);
+      });
+    choice();
+  } catch (err) {
+    console.log('File not found');
+    choice();
+  }
 };
 
 const showAllEmployees = () => {
+  // Showing all the employees
   console.log('\n All Employees: \n-------------------------------');
+  // Showing the employees in a table format
   console.table(employees);
+  // Calling the choice function to ask the user for the next choice
   choice();
 };
 const addEmployee = () => {
+  // Calling the createEmployee function to create the employee
   createEmployee().then((employee) => {
-    employees.push(employee);
+    // Adding the employee to the employees array
+    console.log('Employee added successfully');
+    // Writing the employee to the file
     fs.appendFileSync(PATH, employee.convertToString() + '\n');
+    // Calling the choice function to ask the user for the next choice
+    choice();
   });
-  choice();
 };
 const deleteEmployee = () => {
+  // Reading the email and name of the employee to delete
   reader.question('Enter email of employee to delete: ', (email) => {
-    reader.question('Enter name of employee to delete: ', (name) => {
-      const index = employees.findIndex(
-        (employee) => employee.email === email && employee.name === name
-      );
-      if (index !== -1) {
-        employees.splice(index, 1);
-        fs.writeFileSync(PATH, '');
-        employees.forEach((employee) => {
-          fs.appendFileSync(PATH, employee.convertToString() + '\n');
-        });
-      } else {
-        console.log('Employee not found');
-      }
-      choice();
-    });
+    // Finding the index of the employee to delete
+    const index = employees.findIndex((employee) => employee.email == email);
+    // If the employee is found, deleting the employee
+    if (index !== -1) {
+      employees.splice(index, 1);
+      fs.writeFileSync(PATH, '');
+      employees.forEach((employee) => {
+        fs.appendFileSync(PATH, employee.convertToString() + '\n');
+      });
+      console.log('Employee Deleted Sucessfully!');
+    } else {
+      console.log('Employee not found');
+    }
+    // Calling the choice function to ask the user for the next choice
+    choice();
   });
 };
 const searchEmployee = () => {
-  read.question('\nEnter the text to search: ', (query) => {
-    read.question('Order by (name, email, age, dob): ', (sortBy) => {
-      read.question('Sort direction (asc, desc): ', (sortDirection) => {
+  // Reading the query and the field to search
+  reader.question('\nEnter the text to search: ', (query) => {
+    reader.question('Order by (name, email, age, dob): ', (sortBy) => {
+      reader.question('Sort direction (asc, desc): ', (direction) => {
         //filtering those employees which have the query
         const results = employees
           .filter((employees) => {
             return Object.keys(employees).some(
-              (val) => employees[val] === query
+              (val) => employees[val] == query
             );
           })
-          .sort((first, second) => {
-            let sortOrder = sortDirection === 'asc' ? 1 : -1;
-            if (first[sortBy] < second[sortBy]) {
-              return -1 * sortOrder;
-            } else if (first[sortBy] > first[sortBy]) {
-              return 1 * sortOrder;
+          .sort((a, b) => {
+            let order = direction == 'asc' ? 1 : -1;
+            if (a[sortBy] < b[sortBy]) {
+              return -1 * order;
+            } else if (a[sortBy] > a[sortBy]) {
+              return 1 * order;
             } else {
               return 0;
             }
           });
-        console.log(results);
+        // Showing the results in a table format
+        console.table(results);
+        // Calling the choice function to ask the user for the next choice
         choice();
       });
     });
@@ -135,10 +231,11 @@ function choice() {
         showAllEmployees();
         break;
       case '5':
-        read.close();
+        reader.close();
         process.exit(1);
       default:
         console.log('\nInvalid Choice :(');
+        choice();
         break;
     }
   });
