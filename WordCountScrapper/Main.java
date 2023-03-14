8dd578a3224a6d08083f6df1b6ed016294260908
Chirag Jain <103531814+chiragjain28@users.noreply.github.com>
2023-03-14 23:56:01 +0530

    Update Main.java

--- WordCountScrapper/Main.java
+++ WordCountScrapper/Main.java
@@ -47,10 +47,10 @@ class WebContent {
   }
 
   // Sort the Map by Value
-  public static Map<String, Integer> sortByValue(Map<String, Integer> hm) {
+  public static Map<String, Integer> sortByValue(Map<String, Integer> hashmap) {
     // Create a list from elements of HashMap
     List<Map.Entry<String, Integer>> lists = new LinkedList<Map.Entry<String, Integer>>(
-        hm.entrySet());
+        hashmap.entrySet());
 
     // Sort the lists
     Collections.sort(
@@ -64,11 +64,11 @@ class WebContent {
         });
 
     // put data from sorted list to hashmap
-    Map<String, Integer> temp = new LinkedHashMap<String, Integer>();
-    for (Map.Entry<String, Integer> aa : lists) {
-      temp.put(aa.getKey(), aa.getValue());
+    Map<String, Integer> maplist= new LinkedHashMap<String, Integer>();
+    for (Map.Entry<String, Integer> entryy : lists) {
+      maplist.put(entryy.getKey(), entryy.getValue());
     }
-    return temp;
+    return maplist;
   }
 
 }
@@ -86,6 +86,11 @@ public class Main {
       while (Reader.hasNext()) {
         urls.add(Reader.next());
       }
+      if(urls.size()==0)
+      {
+        System.out.println("No Url Found");
+        return ;
+      }
 
       // step 2 : - Fetch Words from the text file => words.txt
 
@@ -94,7 +99,11 @@ public class Main {
       while (Reader.hasNext()) {
         words.add(Reader.next());
       }
-
+      if(words.size()==0)
+      {
+        System.out.println("No Words Found");
+        return ;
+      }
       // Step 3 : - Found words in URL
 
       WebContent webContentObj = new WebContent(); // Create Object of WebContent Class
@@ -141,7 +150,7 @@ public class Main {
       }
       System.out.println("===========================================");
     } catch (FileNotFoundException e) {
-      System.out.println(e);
+      System.out.println("File Not Found");
     }
   }
 }
