4c4906638a918e7c6c7388d8a1404aa3a29d63ea
apurvaone <57245377+apurvaone@users.noreply.github.com>
2023-03-28 16:04:21 +0530

    Unit tests and validations(name,age,dob) added in code

--- Employee/EmployeeManagement.java
+++ Employee/EmployeeManagement.java
@@ -1,15 +1,18 @@
 import java.io.*;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class Employee  {
     private int id;
     private String name;
     private String email;
     private int age;
-    private String dob;
+    private Date dob;
     
-    public Employee(int id, String name, String email, int age, String dob) {
+    public Employee(int id, String name, String email, int age, Date dob) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -51,18 +54,22 @@ class Employee  {
         this.age = age;
     }
     
-    public String getDob() {
+    public Date getDob() {
         return dob;
     }
     
-    public void setDob(String dob) {
+    public void setDob(Date dob) {
         this.dob = dob;
     }
     
     @Override
     public String toString() {
-        // SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        return "ID: " + id + ", Name: " + name + ", Email: " + email + ", Age: " + age + ", DOB: " + dob;
+
+        
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        dateFormat.setLenient(false);
+        return "ID: " + id + ", Name: " + name + ", Email: " + email + ", Age: " + age + ", DOB: " + dateFormat.format(dob);
+      
     }
 }
 
@@ -73,8 +80,12 @@ public class EmployeeManagement {
     private static final String FILE_NAME = "EmployeeData.txt";
     private static List<Employee> employees;
 
+
+
+
     public static void main(String[] args) {
         employees = loadEmployeesFromFile();
+        
         Scanner scanner = new Scanner(System.in);
         boolean quit = false;
         while (!quit) {
@@ -84,7 +95,13 @@ public class EmployeeManagement {
             System.out.println("3. Search employees");
             System.out.println("4. Display All employees");
             System.out.println("5. Quit");
-            int choice = scanner.nextInt();
+            int choice;
+            try{
+            choice = scanner.nextInt();}
+            catch(InputMismatchException e){
+                System.out.println("Invalid choice");
+                return;
+            }
             scanner.nextLine();
             switch (choice) {
                 case 1:
@@ -110,6 +127,9 @@ public class EmployeeManagement {
     }
 
     private static List<Employee> loadEmployeesFromFile() {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
         List<Employee> employees = new ArrayList<>();
         try (Scanner scanner = new Scanner(new File(FILE_NAME))) {
             while (scanner.hasNextLine()) {
@@ -119,9 +139,17 @@ public class EmployeeManagement {
                     Employee employee = new Employee();
                     employee.setId(Integer.parseInt(parts[0]));
                     employee.setName(parts[1]);
-                    employee.setEmail(parts[2]);
+
+                    
+                    employee.setEmail(parts[2]);    
+                    
                     employee.setAge(Integer.parseInt(parts[3]));
-                    employee.setDob(parts[4]);
+                    try {
+                        employee.setDob(dateFormat.parse(parts[4]));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                        System.out.println( "Invalid Date");
+                    }
                     employees.add(employee);
                 }
             }
@@ -132,9 +160,12 @@ public class EmployeeManagement {
     }
 
     private static void saveEmployeesToFile(List<Employee> employees) {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
         try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME))) {
             for (Employee employee : employees) {
-                writer.println(employee.getId() + "," + employee.getName() + "," + employee.getEmail() + "," + employee.getAge() + "," + employee.getDob());
+                writer.println(employee.getId() + "," + employee.getName() + "," + employee.getEmail() + "," + employee.getAge() + "," + dateFormat.format(employee.getDob()));
             }
         } catch (IOException e) {
             System.out.println("Error saving employees to file");
@@ -142,19 +173,53 @@ public class EmployeeManagement {
     }
 
     private static void addEmployee(Scanner scanner) {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        dateFormat.setLenient(false);
+
         Employee employee = new Employee();
         System.out.print("Enter name: ");
-        employee.setName(scanner.nextLine());
+        String name = scanner.nextLine();
+        if (name.matches("[a-zA-Z ]+") && !name.isEmpty()) {
+            employee.setName(name);
+        } else {
+            System.out.println("Invalid name");
+            return;
+        }
+    
+
+
         System.out.print("Enter email address: ");
-        employee.setEmail(scanner.nextLine());
+        String email = scanner.nextLine();
+        if(isValidEmail(email))
+        employee.setEmail(email);
+        else{
+            System.out.println("Invalid Email");
+            System.exit(0);
+        }
+
+
         System.out.print("Enter age: ");
-        employee.setAge(scanner.nextInt());
+        int age = scanner.nextInt();
         scanner.nextLine();
-        System.out.print("Enter date of birth (): ");
-        employee.setDob(scanner.nextLine());
+        if (age >= 18 && age <= 60) {
+            employee.setAge(age);
+        } else {
+            System.out.println("Invalid age");
+            return;
+        }
+
+        System.out.print("Enter date of birth (dd-mm-yyyy): ");
+        try {
+            employee.setDob(dateFormat.parse(scanner.nextLine()));
+        } catch (ParseException e) {
+            System.out.println("Invalid date format");
+            e.printStackTrace();
+        }
         employee.setId(generateEmployeeId());
         employees.add(employee);
         saveEmployeesToFile(employees);
+
         System.out.println("Employee added successfully");
     }
 
@@ -167,6 +232,8 @@ public class EmployeeManagement {
         }
         return maxId + 1;
     }
+
+
 
     public static void searchEmployees(Scanner scanner) {
     System.out.print("Enter search query: ");
@@ -263,10 +330,26 @@ private static void displayAllEmployees() {
         return;
     }
 
-    System.out.println("List of employees:");
+    System.out.println("List of employees:" + employees.size());
     for (Employee employee : employees) {
         System.out.println(employee.toString());
     }
+}
+
+
+
+public static boolean isValidEmail(String email) {
+    // Regular expression for validating email addresses
+    String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+    
+    // Create a Pattern object with the regex
+    Pattern pattern = Pattern.compile(regex);
+    
+    // Create a Matcher object with the email address and the Pattern
+    Matcher matcher = pattern.matcher(email);
+    
+    // Check if the email address matches the regex
+    return matcher.matches();
 }
 
 }
