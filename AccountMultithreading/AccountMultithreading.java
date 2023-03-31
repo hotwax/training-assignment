2489d2cc7804aa38262ae43f7282d3a1cf407cda
Nidhi pal <nidhipalmms@gmail.com>
2023-03-31 12:11:13 +0530

    Peer review- place try-catch at the right place

--- AccountMultithreading/AccountMultithreading.java
+++ AccountMultithreading/AccountMultithreading.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class AccountMultithreading {
@@ -5,8 +6,6 @@ public class AccountMultithreading {
   public static void main(String[] args) {
 
     try {
-      Scanner sc = new Scanner(System.in);
-
       System.out.println("Enter 1 to withdraw amount in unsafe manner");
       System.out.println("Enter 2 to withdraw amount in safe manner");
       System.out.println("Enter 3 to exit the program");
@@ -18,39 +17,44 @@ public class AccountMultithreading {
         account1.setInitialBalance();
         System.out.println("Current balance: " + account1.getBalance() + "\n");
 
-        int choice = sc.nextInt();
-        switch (choice) {
-          case 1:
-            AccountOverdrawDemo thread1Overdraw = new AccountOverdrawDemo(account1, "person1");
-            AccountOverdrawDemo thread2Overdraw = new AccountOverdrawDemo(account1, "person2");
-            thread1Overdraw.start();
-            thread2Overdraw.start();
-            System.out.println("\n---------------------------\n");
-            break;
+        try {
+          Scanner sc = new Scanner(System.in);
+          int choice = sc.nextInt();
+          switch (choice) {
+            case 1:
+              AccountOverdrawDemo thread1Overdraw = new AccountOverdrawDemo(account1, "person1");
+              AccountOverdrawDemo thread2Overdraw = new AccountOverdrawDemo(account1, "person2");
+              thread1Overdraw.start();
+              thread2Overdraw.start();
+              System.out.println("\n---------------------------\n");
+              break;
 
-          case 2:
-            AccountOverdrawSafeDemo thread1OverdrawSafe = new AccountOverdrawSafeDemo(account1, "person1");
-            AccountOverdrawSafeDemo thread2OverdrawSafe = new AccountOverdrawSafeDemo(account1, "person2");
-            thread1OverdrawSafe.start();
-            thread2OverdrawSafe.start();
-            System.out.println("\n---------------------------\n");
-            break;
+            case 2:
+              AccountOverdrawSafeDemo thread1OverdrawSafe = new AccountOverdrawSafeDemo(account1, "person1");
+              AccountOverdrawSafeDemo thread2OverdrawSafe = new AccountOverdrawSafeDemo(account1, "person2");
+              thread1OverdrawSafe.start();
+              thread2OverdrawSafe.start();
+              System.out.println("\n---------------------------\n");
+              break;
 
-          // in using AccountOverdrawDemo, the sum of totalWithdrawalAmount of thread1 and
-          // thread2 will be > 1000
-          // which is the scenario we need to handle.
-          // in using AccountOverdrawSafeDemo, the sum of totalWithdrawalAmount of thread1
-          // and thread2 will always be = 1000
-          // by using synchronized block.
+            // in using AccountOverdrawDemo, the sum of totalWithdrawalAmount of thread1 and
+            // thread2 will be > 1000
+            // which is the scenario we need to handle.
+            // in using AccountOverdrawSafeDemo, the sum of totalWithdrawalAmount of thread1
+            // and thread2 will always be = 1000
+            // by using synchronized block.
 
-          case 3:
-            System.out.println("---------------------------\n");
-            System.out.println("Program terminated successfully.");
-            return;
+            case 3:
+              System.out.println("---------------------------\n");
+              System.out.println("Program terminated successfully.");
+              return;
 
-          default:
-            System.out.println("---------------------------\n");
-            System.out.println("Please enter a valid number.\n");
+            default:
+              System.out.println("---------------------------\n");
+              System.out.println("Please enter a valid number.\n");
+          }
+        } catch (InputMismatchException e) {
+          System.out.println("Please enter a valid input.");
         }
       }
     } catch (Exception e) {
