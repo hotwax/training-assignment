92eaba159375d721c703b3b75197449a6256fb96
apurvaone <57245377+apurvaone@users.noreply.github.com>
2023-04-20 19:04:24 +0530

    Replced the usage of let with const, made code more declerative

--- WordScraper/WordScraper.js
+++ WordScraper/WordScraper.js
@@ -4,58 +4,69 @@ const cheerio = require('cheerio');
 
 
 // Function to fetch HTML content and extract text
-const fetchTextContent = async (url) => {
-  try {
-    const response = await axios.get(url);
-    const $ = cheerio.load(response.data);
-    const textContent = $('body').text().replace(/\r/g, '').trim();
-    return textContent;
-  } catch (error) {
-    console.error(`Error fetching ${url}: ${error}`);
-    return null;
-  }
-};
+  const fetchTextContent = async (url) => {
+    try {
+      const response = await axios.get(url);
+      return  (cheerio.load(response.data))('body').text().replace(/\r/g, '').trim();
+    } 
+    
+    catch (error) {
+
+      if (error.code === 404){
+        console.log("404 error, Page not exists: "+url);
+        return null;
+      }
+
+      else if (error.code === 'ENOTFOUND'){
+        console.log("No internet connection: ");
+      }
+
+      else
+      console.error(`Invalid url: ${url}: ${error}`);
+      return null;
+    }
+  };
+
 
 
 // Function to read URLs and words from files, and validate them
 function readUrlAndWords(){
 
-  let urlsSet = new Set() ;
-  let wordsSet = new Set();
-
   try{
-  const urls = fs.readFileSync('urls.txt', 'utf-8').trim().replace(/\r/g, '').split('\n');
-  if (urls.length === 0) {
-    console.error('No URLs found in urls.txt');
-    return;
-  }
 
-  // convert urls to set to remove duplicates
-  urlsSet = new Set(urls);
+    const urls = fs.readFileSync('urls.txt', 'utf-8').trim().replace(/\r/g, '').split('\n').filter((url) => url !== '');
+
+    
+    if (urls.length === 0) {
+      console.error('No URLs found in urls.txt');
+      return;
+    }
+
+    
+    const words= fs.readFileSync('words.txt', 'utf-8').trim().replace(/\r/g, '').split('\n').filter((word) => word !== '');
+    if (words.length === 0) {
+      console.error('No words found in words.txt');
+      return;
+    }
+
+  // convert urls and words to sets to remove duplicates
+    const urlsSet = new Set(urls);
+    const wordsSet = new Set(words);  
+
+
+  // call function to process each URL and show top 3 words, and count overall word count
+  getWordsData(urlsSet,wordsSet);
+
 }
 
 catch(err){
+  if (err.code === 'ENOENT')
+    console.log("File not found: "+err.path);
+  else
   console.log(err)
 }
 
-try{
-  const words= fs.readFileSync('words.txt', 'utf-8').trim().replace(/\r/g, '').split('\n');
-  if (words.length === 0) {
-    console.error('No words found in words.txt');
-    return;
-  }
-
-  // convert words to set to remove duplicates
-  wordsSet = new Set(words);  
 }
-catch(err){
-  console.log(err)
-}
-
-getWordsData(urlsSet,wordsSet);
-
-}
-
 
 
 // Function to count occurrences of words in text content
@@ -108,6 +119,5 @@ function showOverallWords(totalWordCount){
   });}
 
   
-
 // Function to start the process
 readUrlAndWords();
