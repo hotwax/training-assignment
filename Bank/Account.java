8ab8e2874eb7b49ab1e647307e30f49246e24ce0
Kunal <kunalbhade2@gmail.com>
2023-06-12 18:57:39 +0530

    used descriptive variable names, Added Screenshots in seperate folders and Proper formatting in code.

--- Bank/Account.java
+++ Bank/Account.java
@@ -1,9 +1,11 @@
- class Account {
+class Account {
 	private int balance = 1000;
+
 	public int getBalance() {
-	return balance;
+		return balance;
 	}
+
 	public void withdraw(int amount) {
-	balance = balance - amount;
+		balance = balance - amount;
 	}
 }
--- Bank/AccountOverdrawSafeDemo.java
+++ Bank/AccountOverdrawSafeDemo.java
@@ -1,55 +1,49 @@
-class AccountOverdrawSafeDemo implements Runnable{
-    private Account account; //instance variable
-    public static void main(String args[])
-    {
+class AccountOverdrawSafeDemo implements Runnable {
+    private Account account; // instance variable
+
+    public static void main(String args[]) {
         Account account = new Account();
         AccountOverdrawSafeDemo accountOverdraw = new AccountOverdrawSafeDemo(account);
-        Thread T1=  new Thread(accountOverdraw);
-        Thread T2= new Thread(accountOverdraw);
-        //Given names to threads
-        T1.setName ("Kunal");
-        T2.setName ("Akhil");
-        //starting thread 
-        T1.start();
-        T2.start();
+        Thread thread1 = new Thread(accountOverdraw);
+        Thread thread2 = new Thread(accountOverdraw);
+        // Give names to threads
+        thread1.setName("Kunal");
+        thread2.setName("Akhil");
+        // Start threads
+        thread1.start();
+        thread2.start();
     }
 
-    public AccountOverdrawSafeDemo(Account account)
-    {
+    public AccountOverdrawSafeDemo(Account account) {
         this.account = account;
     }
 
-    //run method to check current Balance  while Thread is withdrawing
+    // Run method to check current balance while the thread is withdrawing
     @Override
-    public synchronized void run(){
-        for(int index=1;index<=5;index++)
-        {
-            withdraw(200); //withdraw 200 each time
-            if(account.getBalance()< 0)
-            {
+    public synchronized void run() {
+        for (int index = 1; index <= 5; index++) {
+            withdraw(200); // Withdraw 200 each time
+            if (account.getBalance() < 0) {
                 System.out.println("Account is Overdrawn");
             }
         }
     }
-    //withdraw method
-    private void withdraw(int withdrawAmount)
-    {
-        if(account.getBalance()>=withdrawAmount)
-        {
-            System.out.println (Thread.currentThread().getName()+" "+"is going to withdraw"+" "+withdrawAmount);
-            try
-            {
+
+    // Withdraw method
+    private void withdraw(int withdrawAmount) {
+        if (account.getBalance() >= withdrawAmount) {
+            System.out.println(Thread.currentThread().getName() + " is going to withdraw " + withdrawAmount);
+            try {
                 Thread.sleep(1000);
-            }catch(InterruptedException e){
+            } catch (InterruptedException e) {
                 System.out.println("Error");
             }
             account.withdraw(withdrawAmount);
-            System.out.println (Thread.currentThread().getName()+" "+"Completed withdraw of"+" "+withdrawAmount);
+            System.out.println(Thread.currentThread().getName() + " completed withdraw of " + withdrawAmount);
+        } else {
+            System.out.println(
+                    "Not enough amount for " + Thread.currentThread().getName() + " to withdraw: "
+                            + account.getBalance());
         }
-        else
-        { //Printing Balance after withdrawing 
-            System.out.println("Not Enough Amount for" +" "+ Thread.currentThread().getName()+" "+ " to withdraw"+" "+account.getBalance());
-        } 
     }
 }
-
